0ec9d3cb1e44d02090a4261718bded290b09396d
Mahmoud-cell <mtmkcell@gmail.com>
2023-03-12 14:36:26 +0200

    footer-comp

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,11 @@
 import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
 function App() {
   return (
     <div className="layout">
       <Header />
       <main>Content</main>
-      <footer>Footer</footer>
+      <Footer />
     </div>
   );
 }
--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -5,3 +5,4 @@ import "../../css/Header/Header.css";
 export default function Header() {
   return <header>Shopping Cart</header>;
 }
+
